ee5e930edd9cd7a5b12e7c429d49c08a39783b6a
Albert Brand <abrand@xebia.com>
2017-09-29 08:23:00 +0200

    Add firebase config, show label

--- App.js
+++ App.js
@@ -1,14 +1,50 @@
 /* @flow */
 import React from 'react';
 import { Platform, Text, View } from 'react-native';
+import * as firebase from 'firebase';
 
+const config = {
+  apiKey: 'AIzaSyDjXyFPFhBvHWvRBoGm2umbhUGfjEDkZ-E',
+  authDomain: 'albert-brand-speeltuin.firebaseapp.com',
+  databaseURL: 'https://albert-brand-speeltuin.firebaseio.com',
+  projectId: 'albert-brand-speeltuin',
+  storageBucket: 'albert-brand-speeltuin.appspot.com',
+  messagingSenderId: '1017341325599',
+};
+
+// workaround for firebase long timers causing a warning
+(console: Object).ignoredYellowBox = ['Setting a timer'];
+
+type State = {
+  label?: string,
+};
 class App extends React.Component {
+  state: State = {};
+
+  constructor() {
+    super();
+    firebase.initializeApp(config);
+
+    this.labelListener();
+  }
+
   render() {
     return (
-      <View style={[styles.root, styles.center]}>
-        <Text>Hello Mendix meetup visitors!</Text>
+      <View style={styles.root}>
+        <View style={[styles.center]}>
+          <Text style={styles.stateText}>
+            Looking for "{this.state.label}"
+          </Text>
+        </View>
       </View>
     );
+  }
+
+  labelListener() {
+    firebase.database().ref('label').on('value', snapshot => {
+      const label = snapshot.val();
+      this.setState({ label });
+    });
   }
 }
 export default App;
@@ -17,6 +53,9 @@ const styles = {
   root: {
     flex: 1,
     marginTop: Platform.select({ ios: 0, android: 24 }),
+  },
+  stateText: {
+    fontSize: 16,
   },
   center: {
     alignItems: 'center',
